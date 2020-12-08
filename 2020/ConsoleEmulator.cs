65cb0745ea7a148753d8fe3bc20ff7c94c90acb3
Claudiu Guiman <clguiman@microsoft.com>
2020-12-07 21:48:31 -0800

    day 8

---
+++ 2020/ConsoleEmulator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2020
+{
+    internal class ConsoleEmulator
+    {
+        public enum Operation
+        {
+            Invalid,
+            Acc,
+            Jmp,
+            Nop,
+        }
+
+        public struct DecodedInstruction
+        {
+            public Operation Op { get; init; }
+            public int Value { get; init; }
+
+            public static DecodedInstruction Decode(string instr)
+            {
+                var tokens = instr.Split(' ');
+                var op = (tokens[0]) switch
+                {
+                    "acc" => Operation.Acc,
+                    "jmp" => Operation.Jmp,
+                    "nop" => Operation.Nop,
+                    _ => throw new ArgumentException($"Invalid Operation: {tokens[0]}"),
+                };
+                return new DecodedInstruction() { Op = op, Value = int.Parse(tokens[1]) };
+            }
+        }
+
+        public class InfiniteLoopException : Exception
+        {
+            public InfiniteLoopException(string msg) : base(msg)
+            {
+            }
+        }
+
+        public ConsoleEmulator(IEnumerable<string> instructions) : this(instructions.Select(i => DecodedInstruction.Decode(i)))
+        {
+        }
+
+        public ConsoleEmulator(IEnumerable<DecodedInstruction> instructions)
+        {
+            this.instructions = instructions.ToArray();
+        }
+
+        public void Run(bool detectInfiniteLoop)
+        {
+            var executedInstructions = new HashSet<int>();
+            while (ip < instructions.Length)
+            {
+                if (detectInfiniteLoop)
+                {
+                    if (executedInstructions.Contains(ip))
+                    {
+                        throw new InfiniteLoopException($"Instruction on position {ip} ({instructions[ip]}) was already executed!");
+                    }
+                    executedInstructions.Add(ip);
+                }
+
+                Step(instructions[ip]);
+            }
+        }
+
+        private void Step(DecodedInstruction instr)
+        {
+            switch (instr.Op)
+            {
+                case Operation.Acc:
+                    Acc += instr.Value;
+                    ip++;
+                    break;
+                case Operation.Jmp:
+                    ip += instr.Value;
+                    break;
+                default:
+                    ip++;
+                    break;
+            }
+        }
+
+        public int Acc { get; private set; }
+
+        private int ip;
+
+        private readonly DecodedInstruction[] instructions;
+    }
+}
---
+++ 2020/Day08.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace _2020
+{
+    public class Day08
+    {
+        [Fact]
+        public void Test1()
+        {
+            var input = new[] {
+            "nop +0",
+"acc +1",
+"jmp +4",
+"acc +3",
+"jmp -3",
+"acc -99",
+"acc +1",
+"jmp -4",
+"acc +6"};
+            Assert.Equal(5, Part1(input));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1563, Part1(File.ReadAllLines("input/day08.txt")));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var input = new[] {
+            "nop +0",
+"acc +1",
+"jmp +4",
+"acc +3",
+"jmp -3",
+"acc -99",
+"acc +1",
+"jmp -4",
+"acc +6"};
+            Assert.Equal(8, Part2(input));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.Equal(767, Part2(File.ReadAllLines("input/day08.txt")));
+        }
+
+        private static int Part1(IEnumerable<string> input)
+        {
+            var console = new ConsoleEmulator(input);
+            try
+            {
+                console.Run(detectInfiniteLoop: true);
+            }
+            catch (ConsoleEmulator.InfiniteLoopException)
+            {
+                return console.Acc;
+            }
+            return 0;
+        }
+
+        private static int Part2(IEnumerable<string> input)
+        {
+            var instructions = input.Select(i => ConsoleEmulator.DecodedInstruction.Decode(i)).ToArray();
+
+            for (var i = 0; i < instructions.Length; i++)
+            {
+                if (instructions[i].Op == ConsoleEmulator.Operation.Jmp || instructions[i].Op == ConsoleEmulator.Operation.Nop)
+                {
+                    var cpy = new ConsoleEmulator.DecodedInstruction[instructions.Length];
+                    instructions.CopyTo(cpy, 0);
+                    cpy[i] = new ConsoleEmulator.DecodedInstruction()
+                    {
+                        Value = cpy[i].Value,
+                        Op = (cpy[i].Op == ConsoleEmulator.Operation.Jmp) ? ConsoleEmulator.Operation.Nop : ConsoleEmulator.Operation.Jmp
+                    };
+
+                    var console = new ConsoleEmulator(cpy);
+                    try
+                    {
+                        console.Run(detectInfiniteLoop: true);
+                        return console.Acc;
+                    }
+                    catch (ConsoleEmulator.InfiniteLoopException)
+                    {
+
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
