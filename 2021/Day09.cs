ddb5faa70aadde1e6886f7e27d02b7dfea3d8c9f
Claudiu Guiman <clguiman@microsoft.com>
2021-12-09 17:58:34 -0500

    start utils project

--- 2021/Day09.cs
+++ 2021/Day09.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Utils;
 using Xunit;
 
 namespace _2021
@@ -10,49 +11,49 @@ namespace _2021
         [Fact]
         public void Test1()
         {
-            Assert.Equal(15, Part1(new[] {
+            Assert.Equal(15, Part1(new(new[] {
                 "2199943210",
                 "3987894921",
                 "9856789892",
                 "8767896789",
                 "9899965678"
-            }.Select(line => line.Select(c => (int)c - (int)'0').ToArray()).ToArray()));
+            }.Select(line => line.Select(c => (int)c - (int)'0')))));
         }
 
         [Fact]
         public void Test2()
         {
-            Assert.Equal(575, Part1(File.ReadAllLines("input/day09.txt").Select(line => line.Select(c => (int)c - (int)'0').ToArray()).ToArray()));
+            Assert.Equal(575, Part1(new(File.ReadAllLines("input/day09.txt").Select(line => line.Select(c => (int)c - (int)'0')))));
         }
 
 
         [Fact]
         public void Test3()
         {
-            Assert.Equal(1134, Part2(new[] {
+            Assert.Equal(1134, Part2(new(new[] {
                 "2199943210",
                 "3987894921",
                 "9856789892",
                 "8767896789",
                 "9899965678"
-            }.Select(line => line.Select(c => (int)c - (int)'0').ToArray()).ToArray()));
+            }.Select(line => line.Select(c => (int)c - (int)'0')))));
         }
 
         [Fact]
         public void Test4()
         {
-            Assert.Equal(1019700, Part2(File.ReadAllLines("input/day09.txt").Select(line => line.Select(c => (int)c - (int)'0').ToArray()).ToArray()));
+            Assert.Equal(1019700, Part2(new(File.ReadAllLines("input/day09.txt").Select(line => line.Select(c => (int)c - (int)'0')))));
         }
 
-        private static int Part1(int[][] input) => FindLowPoints(input).Select(t => input[t.y][t.x] + 1).Sum();
+        private static int Part1(Grid2D<int> input) => FindLowPoints(input).Select(t => input.At(t.x, t.y) + 1).Sum();
 
-        private static int Part2(int[][] input)
+        private static int Part2(Grid2D<int> input)
         {
             var basinSizes = new List<int>();
             foreach (var (lowPointX, lowPointY) in FindLowPoints(input))
             {
                 var size = 1;
-                var inputCopy = input.Select(l => l.ToArray()).ToArray();
+                var inputCopy = input.Clone();
                 List<(int x, int y)> locationsToVisit = new() { (lowPointX, lowPointY) };
 
                 while (locationsToVisit.Count > 0)
@@ -61,7 +62,7 @@ namespace _2021
                     foreach (var (curX, curY) in locationsToVisit)
                     {
                         newLocations.AddRange(GetAdjacentBasinLocations(inputCopy, curX, curY));
-                        inputCopy[curY][curX] = int.MinValue;
+                        inputCopy.SetAt(int.MinValue, curX, curY);
                     }
                     locationsToVisit = newLocations.Distinct().ToList();
                     size += locationsToVisit.Count;
@@ -71,19 +72,19 @@ namespace _2021
             return basinSizes.OrderByDescending(x => x).Take(3).Aggregate((a, b) => a * b);
         }
 
-        private static IEnumerable<(int x, int y)> GetAdjacentBasinLocations(int[][] input, int locationX, int locationY) =>
-            GetAdjacentLocations(input, locationX, locationY).Where(t => input[t.y][t.x] != 9 && input[t.y][t.x] > input[locationY][locationX]);
+        private static IEnumerable<(int x, int y)> GetAdjacentBasinLocations(Grid2D<int> input, int locationX, int locationY) =>
+            input.GetAdjacentLocations(locationX, locationY).Where(t => input.At(t.x, t.y) != 9 && input.At(t.x, t.y) > input.At(locationX, locationY));
 
-        private static IEnumerable<(int x, int y)> FindLowPoints(int[][] input)
+        private static IEnumerable<(int x, int y)> FindLowPoints(Grid2D<int> input)
         {
-            for (var i = 0; i < input.Length; i++)
+            for (var i = 0; i < input.Height; i++)
             {
-                for (var j = 0; j < input[0].Length; j++)
+                for (var j = 0; j < input.Width; j++)
                 {
                     bool isLowPoint = true;
-                    foreach (var (x, y) in GetAdjacentLocations(input, j, i))
+                    foreach (var (x, y) in input.GetAdjacentLocations(j, i))
                     {
-                        if (input[y][x] <= input[i][j])
+                        if (input.At(x, y) <= input.At(j, i))
                         {
                             isLowPoint = false;
                             break;
@@ -94,26 +95,6 @@ namespace _2021
                         yield return (j, i);
                     }
                 }
-            }
-        }
-
-        private static IEnumerable<(int x, int y)> GetAdjacentLocations(int[][] input, int locationX, int locationY)
-        {
-            if (locationY > 0)
-            {
-                yield return (locationX, locationY - 1);
-            }
-            if (locationY < input.Length - 1)
-            {
-                yield return (locationX, locationY + 1);
-            }
-            if (locationX > 0)
-            {
-                yield return (locationX - 1, locationY);
-            }
-            if (locationX < input[0].Length - 1)
-            {
-                yield return (locationX + 1, locationY);
             }
         }
     }
---
+++ Utils/Grid2D.cs
@@ -0,0 +1,58 @@
+﻿namespace Utils
+{
+    public class Grid2D<T>
+    {
+        public Grid2D(IEnumerable<IEnumerable<T>> input)
+        {
+            _grid = input.Select(l => l.ToArray()).ToArray();
+            Width = _grid[0].Length;
+            Height = _grid.Length;
+        }
+
+        public int Width { get; init; }
+
+        public int Height { get; init; }
+
+        public IEnumerable<T> Enumerate()
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                for (var x = 0; x < Width; x++)
+                {
+                    yield return _grid[y][x];
+                }
+            }
+        }
+
+        public T At(int x, int y) => _grid[y][x];
+
+        public void SetAt(T val, int x, int y)
+        {
+            _grid[y][x] = val;
+        }
+
+        public Grid2D<T> Clone() => new(_grid);
+
+        public IEnumerable<(int x, int y)> GetAdjacentLocations(int locationX, int locationY)
+        {
+            if (locationY > 0)
+            {
+                yield return (locationX, locationY - 1);
+            }
+            if (locationY < Height - 1)
+            {
+                yield return (locationX, locationY + 1);
+            }
+            if (locationX > 0)
+            {
+                yield return (locationX - 1, locationY);
+            }
+            if (locationX < Width - 1)
+            {
+                yield return (locationX + 1, locationY);
+            }
+        }
+
+        private readonly T[][] _grid;
+    }
+}
